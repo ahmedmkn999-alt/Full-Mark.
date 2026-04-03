d1d56dbe0476c05a30763063f5e942babac666cc
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2026-04-03 16:00:10 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,21 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  // إعداد البروكسي لموقع المنصة
   const proxy = createProxyMiddleware({
     target: 'http://fullmark.online',
     changeOrigin: true,
-    pathRewrite: {
-      '^/api/proxy': '', // إعادة توجيه المسارات
-    },
+    autoRewrite: true,
+    followRedirects: true, // عشان لو الموقع عمل Redirect يدخل معاه
     onProxyRes: (proxyRes) => {
-      // حذف قيود الحماية عشان تفتح في أي جهاز (Iframe)
+      // حذف الحماية تماماً عشان المتصفح ميعملش Block
       delete proxyRes.headers['x-frame-options'];
       delete proxyRes.headers['content-security-policy'];
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
     },
+    onProxyReq: (proxyReq) => {
+      // إجبار السيرفر إنه يشوف الطلب كأنه جاي من جهاز كمبيوتر ثابت
+      proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+    }
   });
 
   return proxy(req, res);
